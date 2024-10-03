bb56cb0a4580b3c48d7b090cdff35e55e71504f7
NEVEROV iaroslav <beeguy74@gmail.com>
2024-10-03 16:49:26 +0200

    oi

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ name = st.text_input('What is your name?')
 if st.button('Submit'):
     st.write(f'Hello, {name}!')
 
-    if st.button('Clear'):
-        st.experimental_rerun()
+if st.button('Clear'):
+    st.experimental_rerun()
