a6cbb9a0e927bb2b7422911efee71c025b6a5ee2
NEVEROV iaroslav <beeguy74@gmail.com>
2024-10-03 17:12:20 +0200

    test state with st write

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ if 'text' not in st.session_state:
 if isinstance(st.session_state.text, str):
     st.title(f'{st.session_state.text}')
 
-name = st.text_input(st.session_state.text)
+st.text_input(st.session_state.text)
 
 if st.button('Submit'):
-    st.write(f'Hello, {name}!')
+    st.write(f'Hello, {st.session_state.text}!')
 
 st.session_state.text = name
 
