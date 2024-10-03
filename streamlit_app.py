fbbd6dd8984132cb28dffaf09840bb888d61dd0d
NEVEROV iaroslav <beeguy74@gmail.com>
2024-10-03 17:02:54 +0200

    state

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 st.title('Hello World!')
 
+if 'text' not in st.session_state:
+    st.session_state.text = ""
 
-name = st.text_input('What is your name?')
+name = st.text_area(st.session_state.text)
+
 if st.button('Submit'):
     st.write(f'Hello, {name}!')
 
-if st.button('Clear'):
-    st.experimental_rerun()
+st.session_state.text = name
+
