e757fbc0edf4c52d19d55a53825956c34863d7ab
NEVEROV iaroslav <beeguy74@gmail.com>
2024-10-03 17:08:49 +0200

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ st.title('Hello World!')
 if 'text' not in st.session_state:
     st.session_state.text = ""
 
+if isinstance(st.session_state.sum, str):
+    st.title(f'{st.session_state.sum}')
+
 name = st.text_input(st.session_state.text)
 
 if st.button('Submit'):
