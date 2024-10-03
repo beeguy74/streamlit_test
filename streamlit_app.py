6078964d969fefffc9e26e02e46cfb7c921f6ba1
NEVEROV iaroslav <beeguy74@gmail.com>
2024-10-03 16:49:01 +0200

    rerun?

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.title('Hello World!')
 name = st.text_input('What is your name?')
 if st.button('Submit'):
     st.write(f'Hello, {name}!')
+
+    if st.button('Clear'):
+        st.experimental_rerun()
